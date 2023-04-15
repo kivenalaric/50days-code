25bf2302c2d0e87f0312dfbc31a44a327fbcd977
Kiven Alaric <elmozaro3@gmail.com>
2023-04-15 06:44:18 -0500

    start of 50 days code

---
+++ ExpandingCards_Day1/assets/script.js
@@ -0,0 +1,17 @@
+// const pannels = document.querySelectorAll('.pannel');
+
+// // console.log(pannels)
+
+// pannels.forEach((pannel) => {
+//     pannel.addEventListener('click', () => {
+//         // console.log(pannel)
+//         removeActiveClasses()
+//         pannel.classList.add('active')
+//     })
+// }) 
+
+// function removeActiveClasses() {
+//     pannels.forEach((pannel) => {
+//         pannel.classList.remove('active')
+//     })
+// }
